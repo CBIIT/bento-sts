2360bc18a0e83ce7b13c68b5c0e3f466cff4ad95
bensonml <bensonml@nih.gov>
2021-02-02 17:20:23 -0500

    remove ref to post

--- pysts/app/main/routes.py
+++ pysts/app/main/routes.py
@@ -43,26 +43,9 @@ def before_request():
 @bp.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
-    form = PostForm()
-    if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user, language=language)
-        db.session.add(post)
-        db.session.commit()
-        flash("Your post is now live!")
-        return redirect(url_for("main.index"))
-    page = request.args.get("page", 1, type=int)
-    posts = current_user.followed_posts().paginate(
-        page, current_app.config["POSTS_PER_PAGE"], False
-    )
-    next_url = url_for("main.index", page=posts.next_num) if posts.has_next else None
-    prev_url = url_for("main.index", page=posts.prev_num) if posts.has_prev else None
     return render_template(
         "index.html",
         title="Home",
-        form=form,
-        posts=posts.items,
-        next_url=next_url,
-        prev_url=prev_url,
     )
 
 
