2e40d8886f9011fb836250a6314290a08168ee77
Mark A. Jensen <mark.jensen@nih.gov>
2022-02-10 22:56:46 -0500

    feat(swagger): improve return formats
    for tag queries; update OpenApi description

--- pysts/app/api/routes.py
+++ pysts/app/api/routes.py
@@ -33,13 +33,14 @@ ents = {
 @bp.errorhandler(HTTPException)
 def handle_exception(e):
     response = Response(
-        status = e.code,
+        status = e.code or 500,
         response=json.dumps({
-            "code": e.code,
+            "code": e.code or 500,
             "description": str(e.description),
             })
         )
     response.content_type = "application/json"
+    response.access_control_allow_origin = "*"
     return response
 
 @bp.before_app_request
@@ -50,11 +51,14 @@ def before_request():
 @bp.route('/', methods=['GET'])
 @bp.route('/v1', methods=['GET'])
 def index():
-    return jsonify({
+    response = jsonify({
         "application": "STS",
         "version": "0.1",
         "status": "READY"
     })
+    response.access_control_allow_origin = "*"
+    return response
+
 
 
 @bp.route("/v1/<path:path>", methods=['GET'])
@@ -88,10 +92,12 @@ def query_db(path):
         stmt = stmt + " SKIP {}".format(int(skip))
     if limit:
         stmt = stmt + " LIMIT {}".format(int(limit))
-    # kludge for tagged entities - get the entity label = the node type
-    if (q.toks[-1] == 'entities'):
-        stmt = re.sub("(n([0-9]+) as entity)", "\\1, head(labels(n\\2)) as label",
+    # kludge tag_entities statement to add label
+    if (q.path_id == 'tag_entities'):
+        stmt = re.sub("(n([0-9]+) as entities)",
+                      "\\1, head(labels(n\\2)) as label",
                       stmt)
+
     current_app.logger.info(stmt)
     current_app.logger.info(q.params)
     ret = None
@@ -104,17 +110,31 @@ def query_db(path):
     if len(ret) == 1:
         ret = ret[0]
     else:
-        # array of dicts to dict of arrays
-        xfm = {}
-        if q.toks[-1] == "entities":
+        if q.path_id == "tag_entities":
+            # transform for tagged entities
             for i in range(0,len(ret)):
-                ret[i]['entity']['label'] = ret[i]['label']
+                ret[i]['entities']['label'] = ret[i]['label']
                 del ret[i]['label']
+        # standard transform: array of dicts to dict of arrays
+        xfm = {}
         keys = ret[0].keys()
         for k in keys:
             xfm[k] = [x[k] for x in ret]
         ret = xfm
-        # kludge for tagged entities
+        if q.path_id == "tag_values":
+            # transform for tag values
+            xfm = {
+                "key": None,
+                "values": {}
+                }
+            for t in ret['tags']:
+                if not xfm['key']:
+                    xfm['key'] = t['key']
+                if xfm['values'].get(t['value']):
+                    xfm['values'][t['value']] += 1
+                else:
+                    xfm['values'][t['value']] = 1
+            ret = xfm
 
     if total_rows is not None:
         if total_rows > 0:
@@ -122,5 +142,6 @@ def query_db(path):
         else:
             # ../count was 0
             abort(404,"No items found")
-            # ret = {"total":0}
-    return jsonify(ret)
+    response = jsonify(ret)
+    response.access_control_allow_origin = "*"
+    return response
