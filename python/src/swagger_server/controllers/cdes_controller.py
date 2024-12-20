c77ec585b3d540a515824f38600a771d69a4c729
Nelson Moore <nelsonwmoore@gmail.com>
2024-12-20 11:44:27 -0500

    feat: add cde pvs to swagger_server yaml

---
+++ python/src/swagger_server/controllers/cdes_controller.py
@@ -0,0 +1,12 @@
+def cdes_model_handle_model_version(modelHandle, modelVersion):
+    """
+    Get Permissible Values and Synonyms for a specified model and version.
+
+    :param modelHandle: Handle of model (use /models to find available handles)
+    :type modelHandle: str
+    :param modelVersion: Version of model (use /models to find available versions)
+    :type modelVersion: str
+
+    :rtype: object
+    """
+    return "do some magic!"
